224f18ae4d8d1c9dd7cd4998cf1b844434d89849
berk245 <berk.oezzambak@goliving.eu>
2020-05-15 13:14:48 +0200

    change breakpoints & improve responsiveness

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,17 +7,18 @@ const leftSide = document.querySelector(".leftSide");
 const mainBdy = document.querySelector("#bg-img");
 const icons = document.querySelectorAll("#fab");
 const navItems = document.querySelectorAll(".nav-item");
+const closeIcon = document.querySelector(".menu-close");
 
 let showMenu = false;
 
-window.onload = function() {
+window.onload = function () {
   this.setTimeout(() => {
     this.toggleMenu();
   }, 1000);
 };
 menuBtn.addEventListener("click", toggleMenu);
 mainBdy.addEventListener("onscroll", toggleMenu);
-document.body.onkeydown = function(e) {
+document.body.onkeydown = function (e) {
   toggleMenu();
 };
 
@@ -30,22 +31,22 @@ const Mline = document.querySelectorAll(".m-btn-line");
 let MshowMenu = false;
 
 MmenuBtn.addEventListener("click", mobileMenu);
-
+closeIcon.addEventListener("click", mobileMenu);
 function mobileMenu() {
   if (!MshowMenu) {
     MmenuBtn.classList.add("close");
     Mmenu.classList.add("show");
     MmenuNav.classList.add("show");
-    MnavItems.forEach(item => item.classList.add("show"));
-    Mline.forEach(item => item.classList.add("close"));
+    MnavItems.forEach((item) => item.classList.add("show"));
+    Mline.forEach((item) => item.classList.add("close"));
 
     MshowMenu = true;
   } else {
     MmenuBtn.classList.remove("close");
     Mmenu.classList.remove("show");
     MmenuNav.classList.remove("show");
-    MnavItems.forEach(item => item.classList.remove("show"));
-    MnavItems.forEach(item => item.classList.remove("close"));
+    MnavItems.forEach((item) => item.classList.remove("show"));
+    MnavItems.forEach((item) => item.classList.remove("close"));
 
     MshowMenu = false;
   }
@@ -58,7 +59,7 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
 
-    navItems.forEach(item => item.classList.add("show"));
+    navItems.forEach((item) => item.classList.add("show"));
 
     showMenu = true;
   } else {
@@ -67,7 +68,7 @@ function toggleMenu() {
     menu.classList.remove("show");
     menuNav.classList.remove("show");
 
-    navItems.forEach(item => item.classList.remove("show"));
+    navItems.forEach((item) => item.classList.remove("show"));
 
     showMenu = false;
   }
